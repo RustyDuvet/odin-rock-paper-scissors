0fd95fc28c90c30ec930bbacbfaa9ad7829a5f68
RustyDuvet <295281927+RustyDuvet@users.noreply.github.com>
2026-07-15 18:44:06 +0530

    Basic Implementation

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let humanCount = 0
+let computerCount = 0
+let count = 0
+let results = []
+
+const output = document.querySelector("#text")
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     if (choice < 1) {
@@ -15,43 +22,83 @@ function getHumanChoice() {
 }
 
 function playRound(computerChoice, humanChoice) {
-    humanChoice = humanChoice.toLowerCase()
-    console.log(`You chose: ${humanChoice}`)
-    console.log(`Computer chose: ${computerChoice}`)
+
+    if (count > 5) {
+        return;
+    }
+
+    if (count === 5) {
+        results.forEach((result) => {
+        if (result == "human") {
+            humanCount++;
+        } else if (result == "computer") {
+            computerCount++;
+        }
+    })
+
+    if (humanCount > computerCount) {
+        final.textContent = "Human Won!"
+    } else if (humanCount < computerCount) {
+        final.textContent = "Computer Won!"
+    } else {
+        final.textContent = "It's a Tie!"
+    }
+    
+        return;
+    }
+    
+    output.textContent += `You chose: ${humanChoice}\n`;
+    output.textContent += `Computer chose: ${computerChoice}\n`;
+
+    
+
     if (computerChoice === humanChoice) {
-        console.log("It's a tie!");
+        output.textContent += "It's a tie!\n\n";
         return "tie";
     } else if (computerChoice === "rock" && humanChoice === "paper" || 
         computerChoice === "paper" && humanChoice === "scissors" || 
         computerChoice === "scissors" && humanChoice === "rock") {
-        console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+        output.textContent += `You Win! ${humanChoice} beats ${computerChoice}\n\n`;
         return "human";
     } else {
-        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
-        return "computer"
+        output.textContent += `You Lose! ${computerChoice} beats ${humanChoice}\n\n`;
+        return "computer";
     }
+    
 }
 
-function playGame() {
-    let computerScore = 0;
-    let humanScore = 0;
-    let outcome;
-    for(let i = 0; i < 5; i++) {
-        outcome = playRound(getComputerChoice(), getHumanChoice());
-        if (outcome === "human") {
-            humanScore++;
-        } else if (outcome === "computer") {
-            computerScore++;
-        }
-    }
-    console.log(`Final Score:\nHuman: ${humanScore}\nComputer: ${computerScore}`);
-    if(humanScore > computerScore) {
-        console.log("Human is the final winner!"); 
-    } else if(humanScore < computerScore) {
-        console.log("Computer is the final winner!"); 
-    } else {
-        console.log("It's a tie overall!");
-    }
-}
+const rock = document.querySelector("#rock")
+const paper = document.querySelector("#paper")
+const scissors = document.querySelector("#scissors")
+const buttons = document.querySelector(".buttons")
+const times = document.querySelector("#times")
+const final = document.querySelector("#final")
 
-playGame();
+rock.addEventListener("click",() => {
+    results.push(playRound(getComputerChoice(), "rock"));
+});
+paper.addEventListener("click",() => {
+    results.push(playRound(getComputerChoice(), "paper"));
+});
+scissors.addEventListener("click",() => {
+    results.push(playRound(getComputerChoice(), "scissors"));
+});
+buttons.addEventListener("click", () => {  
+    count++;
+    if (count > 5){
+        return;
+    }
+    times.textContent = `Times: ${count}`
+});
+
+
+
+    
+
+
+
+
+
+
+
+
